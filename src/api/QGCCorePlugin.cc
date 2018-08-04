60e033867c7f1c0d00ae16b03f414c3e6d81a7d0
cswp <nudtcomputer@163.com>
2018-08-04 12:29:22 +0800

    111

--- src/api/QGCCorePlugin.cc
+++ src/api/QGCCorePlugin.cc
@@ -1,7 +1,7 @@
 /****************************************************************************
- *天热网特网特网特
+ *天热网特网特网特253253643643
  *   (c) 2009-2016 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
- *
+ *11111
  * QGroundControl is licensed according to the terms in the file
  * COPYING.md in the root of the source code directory.
  *
